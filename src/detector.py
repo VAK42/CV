3d9c9d3c461360fc72545485018a26a91c9ae762
ヴ・アイン・キエト <vutrandangkhoa7@gmail.com>
2025-06-03 23:31:11 +0700

    OK

--- src/detector.py
+++ src/detector.py
@@ -6,5 +6,6 @@ def load_model(model_path="models/yolo11n.pt"):
   return model
   
 def detect_objects(model, frame):
-  results = model(frame)
+  zframe = cv2.resize(frame, (640, 640))
+  results = model(zframe)
   return results
