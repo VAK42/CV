f3dc71692bf2bd1f4cae7c0d141519e5d520cd24
ヴ・アイン・キエト <vutrandangkhoa7@gmail.com>
2025-06-03 19:26:33 +0700

    VAK

--- src/config.py
+++ src/config.py
@@ -1,3 +1,3 @@
-VIDEO_SOURCE = 0
+VIDEO_SOURCE = "rtsp://admin:@SSA1234@192.168.1.108:554/cam/realmonitor?channel=1&subtype=0&unicast=true&proto=Onvif"
 MODEL_PATH = "models/yolo11n.pt"
 MOVEMENT_THRESHOLD = 20
