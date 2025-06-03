e7054705e6b83558c780d49d9bdf9118919e3fe7
ヴ・アイン・キエト <vutrandangkhoa7@gmail.com>
2025-06-03 19:31:10 +0700

    VAK

--- src/detector.py
+++ src/detector.py
@@ -1,9 +1,12 @@
 from ultralytics import YOLO
+import torch
 
 def load_model(model_path="models/yolo11n.pt"):
   model = YOLO(model_path)
+  model.to("cuda")
   return model
-
+  
 def detect_objects(model, frame):
-  results = model(frame)
+  frame_tensor = torch.from_numpy(frame).to("cuda") if torch.cuda.is_available() else frame
+  results = model(frame_tensor)
   return results
