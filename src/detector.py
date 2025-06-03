06fe1720eaa7ed626324804bb60c7a7372dc1c72
ヴ・アイン・キエト <vutrandangkhoa7@gmail.com>
2025-06-03 23:22:28 +0700

    OK

--- src/detector.py
+++ src/detector.py
@@ -1,5 +1,4 @@
 from ultralytics import YOLO
-import torch
 
 def load_model(model_path="models/yolo11n.pt"):
   model = YOLO(model_path)
@@ -7,6 +6,5 @@ def load_model(model_path="models/yolo11n.pt"):
   return model
   
 def detect_objects(model, frame):
-  frame_tensor = torch.from_numpy(frame).to("cuda") if torch.cuda.is_available() else frame
-  results = model(frame_tensor)
+  results = model(frame)
   return results
