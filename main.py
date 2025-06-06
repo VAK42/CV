4e668bbd2b0c643c8a533210199347ec0deb13a4
ヴ・アイン・キエト <vutrandangkhoa7@gmail.com>
2025-06-07 00:50:44 +0700

    OK

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ from ultralytics import YOLO
 import torch
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = YOLO("yolo11n-pose.pt")
-cap = cv2.VideoCapture("0.mp4")
+model = YOLO("yolo11n-pose_openvino_model/")
+if device.type == "cuda":
+  model.half()
+cap = cv2.VideoCapture("0.mp4", cv2.CAP_FFMPEG)
 if not cap.isOpened():
   exit()
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 360)
+cap.set(cv2.CAP_PROP_FPS, 60)
 skeleton = [
   (0, 1), (0, 2),
   (1, 3), (2, 4),
@@ -28,23 +31,34 @@ while True:
   ret, frame = cap.read()
   if not ret or frame is None:
     break
-  results = model.predict(frame, verbose=False, device="cpu")[0]
+  sframe = cv2.resize(frame, (640, 360))
+  results = model.predict(sframe, imgsz=640, device=device, verbose=False, max_det=20)[0]
   if results.keypoints and results.keypoints.xy is not None and results.keypoints.conf is not None:
-    for kp, conf in zip(results.keypoints.xy, results.keypoints.conf):
+    for kp, conf, box, score in zip(results.keypoints.xy, results.keypoints.conf, results.boxes.xyxy,
+                                    results.boxes.conf):
+      if score < 0.4:
+        continue
       for x, y in kp:
-        cv2.circle(frame, (int(x), int(y)), 3, (0, 255, 255), -1)
+        cv2.circle(sframe, (int(x), int(y)), 2, (0, 255, 255), 1)
       for i, j in skeleton:
         if conf[i] > 0.5 and conf[j] > 0.5:
           x1, y1 = map(int, kp[i])
           x2, y2 = map(int, kp[j])
-          cv2.line(frame, (x1, y1), (x2, y2), (0, 128, 255), 2)
+          cv2.line(sframe, (x1, y1), (x2, y2), (0, 128, 255), 1)
+      x1, y1, x2, y2 = map(int, box)
+      w, h = x2 - x1, y2 - y1
+      aspect_ratio = w / h if h > 0 else 0
+      falling = aspect_ratio > 1.2
+      color = (0, 0, 255) if falling else (0, 255, 0)
+      label = "Falling" if falling else "Normal"
+      cv2.rectangle(sframe, (x1, y1), (x2, y2), color, 1)
+      cv2.putText(sframe, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1, cv2.LINE_AA)
   curr_time = time.time()
   elapsed = curr_time - prev_time
   fps_count = 1 / elapsed if elapsed > 0 else fps_count
   prev_time = curr_time
-  cv2.putText(frame, f"FPS: {fps_count:.2f}", (10, 50),
-              cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-  cv2.imshow("VAK", frame)
+  cv2.putText(sframe, f"FPS: {fps_count:.2f}", (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+  cv2.imshow("VAK", sframe)
   if cv2.waitKey(1) & 0xFF == ord("q"):
     break
 cap.release()
