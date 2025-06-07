3991492426ec56ff0107707fd837f1bf5020b76b
ヴ・アイン・キエト <vutrandangkhoa7@gmail.com>
2025-06-07 14:27:58 +0700

    OK

--- dev/Dachshund.py
+++ dev/Dachshund.py
@@ -10,13 +10,13 @@ DRAW_SKELETON = True
 DRAW_BOX = True
 DRAW_LABEL = True
 core = Core()
-available_devices = core.available_devices
+availableDevices = core.available_devices
 model = YOLO("yolo11n-pose_openvino_model/")
-if 'GPU' in available_devices:
+if 'GPU' in availableDevices:
   if hasattr(model, 'model') and hasattr(model.model, 'ov_model'):
     try:
-      compiled_model = core.compile_model(model.model.ov_model, 'GPU')
-      model.model.ov_model = compiled_model
+      compiledModel = core.compile_model(model.model.ov_model, 'GPU')
+      model.model.ov_model = compiledModel
     except Exception as e:
       print(e)
 else:
@@ -43,57 +43,56 @@ skeleton = [
 ]
 executor = ThreadPoolExecutor(max_workers=4)
 futures = []
-last_results = None
-last_frame_for_draw = None
-prev_time = time.time()
-fps_count = 0.0
+lastResults = None
+lastFrameForDraw = None
+prevTime = time.time()
+fpsCount = 0.0
 while True:
   ret, frame = cap.read()
   if not ret or frame is None:
     break
   sframe = cv2.resize(frame, (640, 360))
-  last_frame_for_draw = sframe.copy()
+  lastFrameForDraw = sframe.copy()
   if len(futures) < 1:
-    futures.append(
-      executor.submit(lambda img: model.predict(img, imgsz=640, verbose=False, max_det=60, half=False)[0], sframe))
+    futures.append(executor.submit(lambda img: model.predict(img, imgsz=640, verbose=False, max_det=60, half=False)[0], sframe))
   for f in futures[:]:
     if f.done():
       try:
-        last_results = f.result()
+        lastResults = f.result()
       except Exception as e:
         print(e)
       futures.remove(f)
-  if last_results and last_results.keypoints and last_results.keypoints.xy is not None and last_results.keypoints.conf is not None:
-    for kp, conf, box, score in zip(last_results.keypoints.xy, last_results.keypoints.conf, last_results.boxes.xyxy,
-                                    last_results.boxes.conf):
+  if lastResults and lastResults.keypoints and lastResults.keypoints.xy is not None and lastResults.keypoints.conf is not None:
+    for kp, conf, box, score in zip(lastResults.keypoints.xy, lastResults.keypoints.conf, lastResults.boxes.xyxy,
+                                    lastResults.boxes.conf):
       if score < 0.4:
         continue
       if DRAW_KEYPOINTS:
         for x, y in kp:
-          cv2.circle(last_frame_for_draw, (int(x), int(y)), 2, (0, 255, 255), 1)
+          cv2.circle(lastFrameForDraw, (int(x), int(y)), 2, (0, 255, 255), 1)
       if DRAW_SKELETON:
         for i, j in skeleton:
           if conf[i] > 0.5 and conf[j] > 0.5:
             x1, y1 = map(int, kp[i])
             x2, y2 = map(int, kp[j])
-            cv2.line(last_frame_for_draw, (x1, y1), (x2, y2), (0, 128, 255), 1)
+            cv2.line(lastFrameForDraw, (x1, y1), (x2, y2), (0, 128, 255), 1)
       x1, y1, x2, y2 = map(int, box)
       w, h = x2 - x1, y2 - y1
-      aspect_ratio = w / h if h > 0 else 0
-      falling = aspect_ratio > 1.2
+      aspectRatio = w / h if h > 0 else 0
+      falling = aspectRatio > 1.2
       color = (0, 0, 255) if falling else (0, 255, 0)
       label = "Falling" if falling else "Normal"
       if DRAW_BOX:
-        cv2.rectangle(last_frame_for_draw, (x1, y1), (x2, y2), color, 1)
+        cv2.rectangle(lastFrameForDraw, (x1, y1), (x2, y2), color, 1)
       if DRAW_LABEL:
-        cv2.putText(last_frame_for_draw, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1, cv2.LINE_4)
-  curr_time = time.time()
-  elapsed = curr_time - prev_time
-  fps_count = 1 / elapsed if elapsed > 0 else fps_count
-  prev_time = curr_time
-  if last_frame_for_draw is not None:
-    cv2.putText(last_frame_for_draw, f"FPS: {fps_count:.2f}", (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
-    cv2.imshow("VAK", last_frame_for_draw)
+        cv2.putText(lastFrameForDraw, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1, cv2.LINE_4)
+  currTime = time.time()
+  elapsed = currTime - prevTime
+  fpsCount = 1 / elapsed if elapsed > 0 else fpsCount
+  prevTime = currTime
+  if lastFrameForDraw is not None:
+    cv2.putText(lastFrameForDraw, f"FPS: {fpsCount:.2f}", (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+    cv2.imshow("VAK", lastFrameForDraw)
   if cv2.waitKey(1) & 0xFF == ord("q"):
     break
 cap.release()
