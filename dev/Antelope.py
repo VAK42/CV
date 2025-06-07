b105f7635be9398c577e9625ab4ac0e1cd5b9667
ヴ・アイン・キエト <vutrandangkhoa7@gmail.com>
2025-06-07 14:37:43 +0700

    OK

--- dev/Antelope.py
+++ dev/Antelope.py
@@ -7,18 +7,18 @@ if not cap.isOpened():
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 fps = cap.get(cv2.CAP_PROP_FPS)
-prev_time = time.time()
-fps_count = 0.0
+prevTime = time.time()
+fpsCount = 0.0
 while True:
   ret, frame = cap.read()
   if not ret or frame is None:
     break
-  curr_time = time.time()
-  elapsed_time = curr_time - prev_time
-  if elapsed_time > 0:
-    fps_count = 1 / elapsed_time
-    prev_time = curr_time
-  cv2.putText(frame, f'FPS: {fps_count:.2f}', (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+  currTime = time.time()
+  elapsedTime = currTime - prevTime
+  if elapsedTime > 0:
+    fpsCount = 1 / elapsedTime
+    prevTime = currTime
+  cv2.putText(frame, f'FPS: {fpsCount:.2f}', (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
   cv2.imshow('VAK', frame)
   if cv2.waitKey(1) & 0xFF == ord('q'):
     break
