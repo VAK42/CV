209ec93865be72817cc35e3cf11f1591ca348528
ヴ・アイン・キエト <vutrandangkhoa7@gmail.com>
2025-06-07 14:32:34 +0700

    OK

--- dev/Cheetah.py
+++ dev/Cheetah.py
@@ -9,13 +9,13 @@ DRAW_SKELETON = True
 DRAW_BOX = True
 DRAW_LABEL = True
 core = Core()
-available_devices = core.available_devices
+availableDevices = core.available_devices
 model = YOLO("yolo11n-pose_openvino_model/")
-if 'GPU' in available_devices:
+if 'GPU' in availableDevices:
   if hasattr(model, 'model') and hasattr(model.model, 'ov_model'):
     try:
-      compiled_model = core.compile_model(model.model.ov_model, 'GPU')
-      model.model.ov_model = compiled_model
+      compiledModel = core.compile_model(model.model.ov_model, 'GPU')
+      model.model.ov_model = compiledModel
     except Exception as e:
       print(e)
 else:
@@ -40,8 +40,8 @@ skeleton = [
   (12, 14), (14, 16),
   (11, 12), (5, 6)
 ]
-prev_time = time.time()
-fps_count = 0.0
+prevTime = time.time()
+fpsCount = 0.0
 while True:
   ret, frame = cap.read()
   if not ret or frame is None:
@@ -64,19 +64,19 @@ while True:
             cv2.line(sframe, (x1, y1), (x2, y2), (0, 128, 255), 1)
       x1, y1, x2, y2 = map(int, box)
       w, h = x2 - x1, y2 - y1
-      aspect_ratio = w / h if h > 0 else 0
-      falling = aspect_ratio > 1.2
+      aspectRatio = w / h if h > 0 else 0
+      falling = aspectRatio > 1.2
       color = (0, 0, 255) if falling else (0, 255, 0)
       label = "Falling" if falling else "Normal"
       if DRAW_BOX:
         cv2.rectangle(sframe, (x1, y1), (x2, y2), color, 1)
       if DRAW_LABEL:
         cv2.putText(sframe, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1, cv2.LINE_4)
-  curr_time = time.time()
-  elapsed = curr_time - prev_time
-  fps_count = 1 / elapsed if elapsed > 0 else fps_count
-  prev_time = curr_time
-  cv2.putText(sframe, f"FPS: {fps_count:.2f}", (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+  currTime = time.time()
+  elapsed = currTime - prevTime
+  fpsCount = 1 / elapsed if elapsed > 0 else fpsCount
+  prevTime = currTime
+  cv2.putText(sframe, f"FPS: {fpsCount:.2f}", (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
   cv2.imshow("VAK", sframe)
   if cv2.waitKey(1) & 0xFF == ord("q"):
     break
